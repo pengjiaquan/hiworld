0baaf81fc8ff3d4f3f903e54e34d495cccf627cc
admin@1998 <2441309535@qq.com>
2020-11-27 00:15:44 +0800

    https://github.com/pengjiaquan/hiworld.git

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,7 +1,7 @@
 public class HiWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hi World!");
+        System.out.println("Hi!");
         System.out.println("Hi World-2.0");
     }
 
